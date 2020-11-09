a56528cf0cde97c15f4f16a8bb997ecfd835087d
LukeYaxley <66384053+LukeYaxley@users.noreply.github.com>
2020-11-09 14:29:08 +0000

    Initial commit

---
+++ Main.py
@@ -0,0 +1,38 @@
+#import bs4
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Firefox()
+driver.get("https://www.linkedin.com")
+
+def login():
+    username = driver.find_element_by_id("session_key")
+    username.clear()
+    username.send_keys("lukeyaxley21@gmail.com")
+
+    password = driver.find_element_by_name("session_password")
+    password.clear()
+    password.send_keys("Toa12345")
+    driver.find_element_by_class_name("sign-in-form__submit-button").click()
+    driver.get("https://www.linkedin.com/mynetwork/")
+    driver.maximize_window()
+
+def main():
+    clicked_buttons = []
+    buttons = driver.find_elements_by_class_name("mt2")
+    for i in range(16):
+        if buttons[i-1].id in clicked_buttons:
+            continue
+        else:
+            clicked_buttons.append(buttons[i-1].id)
+            buttons[i-1].click()
+    time.sleep(1)
+    driver.refresh()
+
+#def main():
+
+login()
+time.sleep(5)
+while __name__ == "__main__":
+    main()
+    time.sleep(4)
